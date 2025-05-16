ea7d50cdc4367694c9e1980b90bac1486ffd03fa
dharanika <dharanikar9@gmail.com>
2025-05-16 15:59:48 +0530

    first commit

---
+++ New folder/script.js
@@ -0,0 +1,2 @@
+var a=99;
+   console.log(a);
---
+++ script.js
@@ -0,0 +1,15 @@
+var a=49;
+var b=789;
+  console.log(a);
+  console.log(b);
+  console.log(a+b);
+  console.log(a*b);
+  console.log(a-b);
+  console.log(a/b);
+function()add{
+}
+  var a=60;
+  var b=90;
+  console.log(a+b);
+  
+  
